07353fd0a621262056a4f9557783e435e514c24b
muro3 <muromuro3@gmail.com>
2019-08-29 11:13:40 +0900

    作業開始

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" href="" />
+	<script type="text/javascript" src=""></script>
+	<link rel="stylesheet" href="" type="text/css" media="all" />
+	<title>めにぃふぁぼ</title>
+</head>
+<body>
+
+	<p>test</p>
+
+</body>
+</html>
