948c816506ff0c73e3a950e6874b12225afcae92
muro3 <muromuro3@gmail.com>
2019-08-29 12:55:53 +0900

    ツイート取得テスト

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support("title-tag");
 
 function include_smarty() {
 	if(!class_exists("Smarty")) {
-		require_once(get_template_directory()."/smarty/Smarty.class.php");
+		require_once(get_template_directory()."/lib/smarty/Smarty.class.php");
 	}
 	$smarty = new Smarty();
 	$smarty->template_dir = get_template_directory() . "/templates";
@@ -21,3 +21,49 @@ add_action("wp_enqueue_scripts", function() {
 	wp_enqueue_script("twitter", "//platform.twitter.com/widgets.js");
 	//wp_enqueue_script("script", get_template_directory_uri() . "/assets/script.js");
 });
+
+// TwitterAPIのコード。絶対にgitignoreすること。
+require_once(get_template_directory()."/twitter.php");
+
+// TwitterOAuthクラスをインポート
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+function include_twitter() {
+	// TwitterOAuthライブラリを読み込み
+	require get_template_directory().'/lib/twitteroauth/autoload.php';
+
+	// Twitter APIを利用する認証情報。xxxxxxxxの箇所にそれぞれの情報を指定
+	$CK = TWITTER_CK; // Consumer Keyをセット
+	$CS = TWITTER_CS; // Consumer Secretをセット
+	$AT = TWITTER_AT; // Access Tokenをセット
+	$AS = TWITTER_AS; // Access Token Secretをセット
+
+	// TwitterOAuthクラスのインスタンスを作成
+	$connect = new TwitterOAuth( $CK, $CS, $AT, $AS );
+	return $connect;
+}
+
+function debug($val) {
+	echo "<pre><code>";
+	var_dump($val);
+	echo "</code></pre>";
+}
+
+function test() {
+	$connect = include_twitter();
+	$statuses = $connect->get(
+		'statuses/user_timeline',
+		// 取得するツイートの条件を配列で指定
+		array(
+			// ユーザー名（@は不要）
+			'screen_name' => 'wiredpunch',
+			// ツイート件数
+			'count' => '5',
+			// リプライを除外するかを、true（除外する）、false（除外しない）で指定
+			'exclude_replies' => 'true',
+			// リツイートを含めるかを、true（含める）、false（含めない）で指定
+			'include_rts' => 'false'
+		)
+	);
+	debug($statuses);
+}
