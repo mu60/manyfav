accf569d7b40b8ea9bb60716178db93192ae5ead
muro3 <muromuro3@gmail.com>
2019-08-29 19:31:30 +0900

    最大件数は200までしかできないので変更

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ if(!filter_input(INPUT_GET, "screen_name")) {
 	$ranking = "ユーザ名が入力されていません。";
 } elseif(!filter_input(INPUT_GET, "count")) {
 	$ranking = "ツイート取得数が入力されていません。";
-} elseif(filter_input(INPUT_GET, "count") < 0 || filter_input(INPUT_GET, "count") > 1000) {
-	$ranking = "ツイート取得数は1～1000までです。";
+} elseif(filter_input(INPUT_GET, "count") < 0 || filter_input(INPUT_GET, "count") > 200) {
+	$ranking = "ツイート取得数は1～200までです。";
 } elseif(!ctype_digit(filter_input(INPUT_GET, "count"))) {
 	$ranking = "ツイート取得数は半角数字で入力してください。";
 } elseif(filter_input(INPUT_GET, "screen_name") && filter_input(INPUT_GET, "count")) {
